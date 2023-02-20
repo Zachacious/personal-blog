a8fa329f4b16b639888b48a099c4ae278962abcd
zachacious <back4moore@gmail.com>
2023-02-19 21:54:44 -0600

    added custom notifier store lib

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,29 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  // daisyUI config (optional)
+  daisyui: {
+    // https://share.frontender.app/RY4WmZJ05
+    "mytheme-dark": {
+      primary: "#837feeff",
+      secondary: "#e4dedeff",
+      accent: "#c71265ff",
+      neutral: "#39224aff",
+      "base-100": "#0d1217",
+      info: "#6e9acbff",
+      success: "#8cd18cff",
+      warning: "#f0c352ff",
+      error: "#b00d34ff",
+    },
+
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "mytheme-dark",
+  },
 };
