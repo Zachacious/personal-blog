5b46a0d728aceb11cf9e18574cf8dd6d7b944afd
zachacious <back4moore@gmail.com>
2023-02-20 22:07:33 -0600

    fixed sidebar layout issue with theme toggle - apply custom theme correctly:

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,29 +4,38 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+
   // daisyUI config (optional)
   daisyui: {
     // https://share.frontender.app/RY4WmZJ05
-    "mytheme-dark": {
-      primary: "#837feeff",
-      secondary: "#e4dedeff",
-      accent: "#c71265ff",
-      neutral: "#39224aff",
-      "base-100": "#0d1217",
-      info: "#6e9acbff",
-      success: "#8cd18cff",
-      warning: "#f0c352ff",
-      error: "#b00d34ff",
-    },
+    themes: [
+      {
+        mydark: {
+          primary: "#837feeff",
+          secondary: "#e4dedeff",
+          accent: "#c71265ff",
+          // neutral: "#0d1217",
+          neutral: "#39224aff",
+          // "base-100": "#39224aff",
+          "base-100": "#0d1217",
+          info: "#6e9acbff",
+          success: "#8cd18cff",
+          warning: "#f0c352ff",
+          error: "#b00d34ff",
+        },
+      },
+      "light",
+    ],
 
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "mytheme-dark",
+    // styled: true,
+    // themes: true,
+    // base: true,
+    // utils: true,
+    // logs: true,
+    // rtl: false,
+    // prefix: "",
+    darkTheme: "mydark",
   },
+
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
