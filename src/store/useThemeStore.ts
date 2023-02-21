cf74c193da37bb3a96d5c7852680e42597c76c66
zachacious <back4moore@gmail.com>
2023-02-20 21:13:34 -0600

    added dark theme toggle and logic

--- src/store/useThemeStore.ts
+++ src/store/useThemeStore.ts
@@ -1,19 +1,36 @@
 import type { ValueNotifierStore } from "./valueNotifierStore";
 import createValueNotifierStore from "./valueNotifierStore";
 
-const state = {
+const initialState = {
   theme: "light",
 };
 
-let store = <ValueNotifierStore<typeof state>>{};
+let store = <ValueNotifierStore<typeof initialState>>{};
 
 const useThemeStore = () => {
-  // if store is empty, initialize it
-  if (Object.keys(store).length === 0) {
-    store = createValueNotifierStore("theme");
+  // system theme
+  const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+
+  if (darkThemeMq.matches) {
+    initialState.theme = "dark";
   }
 
-  return store;
+  // if store is empty, initialize it
+  if (Object.keys(store).length === 0) {
+    store = createValueNotifierStore("theme", initialState, initialState);
+  }
+
+  const toggleTheme = () => {
+    const state = store.get();
+    console.log("state", state);
+    state.theme = state.theme === "light" ? "dark" : "light";
+    store.set(state);
+  };
+
+  return {
+    toggleTheme,
+    value: store,
+  };
 };
 
 export default useThemeStore;
