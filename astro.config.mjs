9c711ec14291a2ea11c890dd36edcd73d02ec55f
zachacious <back4moore@gmail.com>
2023-03-12 12:15:31 -0500

    added navmenu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), pagefind()],
 });
